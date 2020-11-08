73ccc42e52ebde4f0d198c420b25ea5c8a67973c
Albert Gevorgyan <ablertus@yahoo.com>
2020-11-08 23:40:36 +0000

    2: reordered

--- src/matrix.rs
+++ src/matrix.rs
@@ -5,6 +5,15 @@ pub type Idx = (usize, usize);
 
 pub type Matrix = Array2<Element>;
 
+pub fn of(num_rows: usize, num_columns: usize) -> Matrix {
+    Matrix::from_elem((num_rows, num_columns), Element::default())
+}
+
+pub fn from_elements<V>(elements: &[V]) -> Matrix
+    where V: Clone + FixedInitializer<Elem=Element> {
+    arr2(&elements)
+}
+
 pub fn move_back(element: &Element, position: Idx) -> Idx {
     let (row, column) = position;
     match element.pointer {
@@ -14,13 +23,3 @@ pub fn move_back(element: &Element, position: Idx) -> Idx {
         _ => unreachable!()
     }
 }
-
-pub(crate) fn of(num_rows: usize, num_columns: usize) -> Matrix {
-    Matrix::from_elem((num_rows, num_columns), Element::default())
-}
-
-pub fn from_elements<V>(elements: &[V]) -> Matrix
-    where V: Clone + FixedInitializer<Elem=Element> {
-    arr2(&elements)
-}
-
